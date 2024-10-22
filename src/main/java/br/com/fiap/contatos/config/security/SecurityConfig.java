596b42e29639d8283411e92535e8d75392c96000
Guilherme Norio Haguimoto <guinorio@protonmail.com>
2024-10-22 20:11:05 -0300

    Criação da camada de segurança Http

--- src/main/java/br/com/fiap/contatos/config/security/SecurityConfig.java
+++ src/main/java/br/com/fiap/contatos/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,22 +21,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         // # Desabilita o Cross-site Request Forgery.
-        return httpSecurity.csrf(csrf -> csrf.disable())
+         httpSecurity.csrf(AbstractHttpConfigurer::disable);
                 // # Ele configura a autenticação para o tipo STATELESS.
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
                 // # Configuração das autorizações de requisições Http.
-                .authorizeHttpRequests(authorize -> authorize
-                        // # Configura que tipo de método http vai ser configurado
+                httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        // #  .requestMatchers Configura que tipo de método http vai ser configurado
                         // e o local da EndPoint a ser configurado
-                        .requestMatchers(HttpMethod.GET, "/api/contatos")
-                        // # Ele permite que todos os roles façam essa requisição.
-                        .permitAll()
+                        // # O .permitAll() permite que todos os roles façam essa requisição.
+                        .requestMatchers(HttpMethod.GET, "/api/contatos").permitAll()
+                        .requestMatchers(HttpMethod.POST, "api/contatos").permitAll()
                         // # E todos os outros tipo de requests
                         .anyRequest()
                         // # Precisa de autenticação para ser realizado.
                         .authenticated()
-                )
+                );
                 // # Use o .build para finalizar a construção do método.
-                .build();
+                return httpSecurity.build();
     }
 }
