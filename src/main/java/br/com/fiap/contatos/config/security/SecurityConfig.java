e996b830b2e71e04d1612763ba6df120ad012a7a
Guilherme Norio Haguimoto <guinorio@protonmail.com>
2024-10-23 21:18:52 -0300

    Criação da camada de segurança Http

--- src/main/java/br/com/fiap/contatos/config/security/SecurityConfig.java
+++ src/main/java/br/com/fiap/contatos/config/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.fiap.contatos.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -40,4 +42,9 @@ public class SecurityConfig {
                 // # Use o .build para finalizar a construção do método.
                 return httpSecurity.build();
     }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }
--- src/main/java/br/com/fiap/contatos/controller/AuthController.java
+++ src/main/java/br/com/fiap/contatos/controller/AuthController.java
@@ -2,8 +2,12 @@ package br.com.fiap.contatos.controller;
 
 import br.com.fiap.contatos.dto.UsuarioCadastroDto;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid UsuarioCadastroDto usuarioCadastroDto) {
 
@@ -20,6 +27,8 @@ public class AuthController {
                 usuarioCadastroDto.email(),
                 usuarioCadastroDto.senha()
         );
+        Authentication auth = authenticationManager.authenticate(usernamePassword);
 
+        return ResponseEntity.ok().build();
     }
 }
